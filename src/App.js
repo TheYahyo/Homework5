3e51f850402c87ff24ef0e7570b9194b9d4bd027
TheYahyo <oktamjanovyahyo11@gmail.com>
2024-07-14 22:20:10 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Count from './pages/Count';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/users/:id' element={<Count />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
